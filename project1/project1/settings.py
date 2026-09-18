b6d7d5a2fac129dd336e456bb4b51d928ad69250
prasha-9 <p90448704@gmail.com>
2026-09-18 15:37:05 +0530

    Read database and security settings from environment variables

--- project1/project1/settings.py
+++ project1/project1/settings.py
@@ -22,13 +22,13 @@ GEMINI_API_KEY = os.getenv('GEMINI_API_KEY')
 # See https://docs.djangoproject.com/en/6.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-za+&cx@h+sod+(d77&f2$!+_tw_n8wx8u&#s#zvsb=_#ul^!ud'
+SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 RAILRADAR_API_KEY = os.getenv('RAILRADAR_API_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv('DJANGO_DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.getenv('DJANGO_ALLOWED_HOSTS', '').split(',')
 
 
 # Application definition
@@ -75,15 +75,17 @@ WSGI_APPLICATION = 'project1.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/6.1/ref/settings/#databases
-
 DATABASES = {
     'default': {
-         'ENGINE': 'django.db.backends.mysql',
-         'NAME': 'smart_journey_assistant',
-        'USER': 'root',
-        'PASSWORD': 'Prasha@8184',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'HOST': os.getenv('DB_HOST', 'localhost'),
+        'PORT': os.getenv('DB_PORT', '3306'),
     }
 }
+
 
 
 # Password validation
